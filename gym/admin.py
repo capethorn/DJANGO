068185a3be2d492f884a9d0e3e2e9c51d70043ea
grisha <grisha@DESKTOP-SSK063D>
2026-09-16 12:36:41 +0800

    Создал приложение в проекте

--- gym/admin.py
+++ gym/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from gym.models import Subscription
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
--- gym/models.py
+++ gym/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 class Subscription(models.Model):
-    name = models.TextField
-    options
+    name = models.TextField("Название")
+    price = models.IntegerField("Цена")
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+
+# class Option(models.Model):знер
+#     name = models.TextField("Название")
+    
+# class OptionToSubscription(models.Model):
+#     subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+#     option = models.ForeignKey(Option, on_delete=models.CASCADE)
